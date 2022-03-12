2beb2288cbd3309405f9877539d8f05030ae216e
guillermo grillo <guillermo.grillo88@gmail.com>
2022-03-11 22:20:15 -0300

    structure main app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up the app")
+	return nil
+}
+
 func main() {
 	fmt.Println("Comments service")
+
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting app")
+		fmt.Println(err)
+	}
 }
